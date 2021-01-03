a2fdf7a218d4a045ecaeafad9c2761d02e85d5d9
dbarochiya <dhruv.barochia34788@gamil.com>
2021-01-03 16:13:07 +0530

    Delete all the things.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Dhruv Barochiya's blog
-[![Netlify Status](https://api.netlify.com/api/v1/badges/c121e552-e045-412a-ac99-f0e29124fa19/deploy-status)](https://app.netlify.com/sites/dbarochiya/deploys)
--- src/components/wrap-root-element.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { MDXProvider } from '@mdx-js/react'
-import Embed from './embed'
-
-// eslint-disable-next-line import/prefer-default-export
-export const wrapRootElement = ({ element }) => (
-  <MDXProvider components={{ Embed }}>{element}</MDXProvider>
-)
